6a29eb3263ab5ff1932029f9ce1d56db5ccf372c
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-01-23 18:13:18 +0530

    just comment changes

--- 11_To_Do_Auth/index.js
+++ 11_To_Do_Auth/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express();
+const PORT = 3000;
 const jwt = require('jsonwebtoken');
 app.use(express.json());
 const JWT_SECRET = "dheerajp45";
 const users =[];
+const todos = [];
+let todoId = 0;
+
+function auth(req,res,next){
+    const token = req.headers.token
+    try {
+        const decoded_info = jwt.verify(token,JWT_SECRET)
+        if (decoded_info.username) {
+            req.username = decoded_info.username;
+            next();
+        }
+        else{
+            res.json({
+                message:"you are not logged in"
+            })
+        }
+    } catch (error) {
+        res.status(401).json({
+            message:"invalid token"
+        })
+    }
+}
 
 app.post('/signup',function (req,res){
 const username=req.body.username;
@@ -14,7 +37,7 @@ for (let i = 0; i < users.length; i++) {
     if(users[i].username==username)
     {
         userExists =true;
-    }
+    }}
     if (userExists) {
         res.json({
             message:"There is an account already created with this username please sign in with your account"
@@ -32,7 +55,7 @@ for (let i = 0; i < users.length; i++) {
             
         }
     
-}
+
 })
 
 app.post('/signin',function (req,res){
@@ -40,4 +63,83 @@ const username=req.body.username;
 const password=req.body.password;
 
 let foundUser = null;
+
+for (let i = 0; i < users.length; i++) {
+    if(users[i].username==username && users[i].password == password)
+    {
+        foundUser = users[i]
+    }}
+    if(foundUser){
+        const token = jwt.sign({
+            username:username
+        },JWT_SECRET);
+        res.json({
+            token:token
+        })
+    }
+    else
+    {
+        res.status(403).send({
+            message:"invalid username or pwd"
+        })
+    }
+    console.log(users);
+    
 })
+
+app.post('/todos',auth,function(req,res){
+    const title = req.body.title;
+    todoId = todoId+1;
+    todo={
+        id:todoId,
+        title:title,
+        completed:false,
+        username:req.username
+    }
+    todos.push(todo)
+    res.json({
+        message:`done creating a todo 
+        id -->${todoId}
+        title -->${title}
+        username -->${req.username}
+        ------
+        `
+    })
+    console.log(todos)
+})
+
+app.get('/todos',auth,function(req,res){
+    username= req.username;
+    
+    let user_todos=[];
+    for(let i=0;i<todos.length;i++)
+        if (todos[i].username==username) {
+            user_todos.push(todos[i])
+        }
+        res.json({ todos: user_todos });
+})
+
+app.put('/todos/:id',auth,function(req,res){
+    username= req.username;
+    let updated = false;
+    const id = parseInt(req.params.id);
+    for(let i=0;i<todos.length;i++){
+        if(todos[i].id===id&&todos[i].username==username){
+            todos[i].completed=true;
+            updated=true;
+            break;
+        }
+    }
+    if (updated) {
+    res.json({ message: "Todo marked as completed." });
+} else {
+    res.status(404).json({ message: "Todo not found or not yours." });
+}
+
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
