fbf84b3431590c1c6db14b495d2d25b566c4abf4
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-02-25 19:49:58 +0530

    added skeleton routes

--- 13_course_selling/index.js
+++ 13_course_selling/index.js
@@ -5,6 +5,43 @@ app.get("/",function(req,res){
     res.send("<h1>hi hello from the server</h1>")
 })
 
+app.post("/user/signup",function(req,res){
+})
+
+app.post("/user/signin",function(req,res){
+})
+
+app.post("/user/purchases",function(req,res){
+})
+
+app.get("/courses",function(req,res){
+})
+
+app.get("/course/purchase",function(req,res){
+})
+
+
+
+
+
+app.post("/adminSignup",function(req,res){
+})
+
+app.post("/adminLogin",function(req,res){
+})
+
+app.post("/courseCreate",function(req,res){
+})
+
+app.delete("/courseDelete",function(req,res){
+})
+
+app.put("/addContent",function(req,res){
+})
+
+
+
+
 app.listen(port,()=>{
     console.log("server started on port 3000");
     
