c0e3e68c13c5d45f427dd71ab4170cccc68966aa
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-03-10 09:50:09 +0530

    did soem changes

--- 15_useEffect/useEffect_practice/src/App.jsx
+++ 15_useEffect/useEffect_practice/src/App.jsx
@@ -5,26 +5,45 @@ import './App.css'
 function App(){
   
 const [count , setCount] = useState(0);
+const [count2 , setCount2] = useState(0);
 
   function inc_Count(){
         setCount(prev=> prev+1)
     }
+  function dec_Count(){
+        setCount2(prev=> prev-1)
+    }
 
 return <div>
-    <Counter count={count}></Counter>
+    <Counter count={count} count2={count2}></Counter>
     <button onClick={inc_Count}>increse the count</button>
+    <br /><br />
+        <button onClick={dec_Count}>decrese the count</button>
 </div>
 }
 
 function Counter(props){
     useEffect(()=>{
-        console.log("mount");
-            return function(){
+        console.log("mount"); 
+        return function(){
       console.log("unmount");
     }
 },[]);
+useEffect(()=>{
+    console.log("count has changed");
+
+    return ()=>{
+        console.log(("inside clean up of first count"));
+        
+    }
+},[props.count])
+// useEffect(()=>{
+//     console.log("count2 has changed");
+// },[props.count2])
     return <div>
-        counter {props.count}
+        counter1 =  {props.count}
+        <br /><hr />
+        counter2 = {props.count2}
     </div>
 }
 export default App
