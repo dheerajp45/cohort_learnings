dbb041f82213920a89d554d93c15328246f901b5
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-05-19 17:58:07 +0530

    did some useref

--- 19_useRef/src/App.jsx
+++ 19_useRef/src/App.jsx
@@ -1,6 +1,7 @@
 import { Clock } from "./Clock"
 // import { Signup } from "./Signup"
 function App() {
+
   return <div>
   <Clock></Clock>
   </div> 
--- 19_useRef/src/Clock.jsx
+++ 19_useRef/src/Clock.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function Clock(){
     const [currentCount,setcurrentCount] = useState(0);
-    
+    // const [timer , setTimer]=useState(0)
+    const timer = useRef();
+    // let timer = 0;
     function startClock(){
-        setInterval(()=>{
+         let value= setInterval(()=>{
             setcurrentCount(c=> c+1)
         },1000)
+        // setTimer(value);
+        timer.current = value;
+    }
+    function stopClock(){
+        // clearInterval(timer);
+        clearInterval(timer.current)
     }
     return <div><center>
         <b>
         {currentCount}
          </b> <br />
          <button onClick={startClock}><b>Start</b></button>
-        <button><b>Stop</b></button>
+        <button onClick={stopClock}><b>Stop</b></button>
 
     </center></div>
 }
