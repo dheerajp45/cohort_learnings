b99ee34512281192bd513798ade119fa49239a99
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-05-27 03:16:05 +0530

    asyc queries, atom & selector family,loadbales

---
+++ 24_fe_be/atom-family/src/todos.js
@@ -0,0 +1,14 @@
+export const TODOS = [{
+    id: 1,
+    title: "Go to Gym ",
+    description: "Do the arms"
+}, {
+    id: 2,
+    title: "Go to eat food ",
+    description: "Todays special is Chicken Biryani "
+},
+{
+    id: 3,
+    title: "Read a Book ",
+    description: "Atleast read 5-6 pages "
+},]
