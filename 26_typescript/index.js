0aa0c5da50dd5d1644f7a07b146bc1fbaaccb929
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-06-08 22:43:18 +0530

    interface introdcction

--- 26_typescript/index.js
+++ 26_typescript/index.js
@@ -1,5 +1,38 @@
 "use strict";
+// let x: number = 1;
 Object.defineProperty(exports, "__esModule", { value: true });
-let x = 1;
-console.log(x);
+// console.log(x)
+// function greet(name:String){
+//     console.log("hello -> "+name);
+// }
+// greet("dheeraj")
+// function sum(a:number , b:number): number{
+//     return a+b
+// }
+// console.log(sum(1,2));
+// function delayedCall(fn:()=> void){
+//     setTimeout(fn,1000)
+// }
+// delayedCall(function(){
+//     console.log("hello");
+// })
+function greet(user) {
+    console.log("hello " + user.name);
+}
+let user = {
+    name: "dheeraj",
+    age: 19
+};
+greet(user);
+function register(event) {
+    console.log(`congratulation you have registered for the ${event.eventName}
+        event mode is ${event.eventMode}
+        with the capacity of ${event.eventCapacity}`);
+}
+let event1 = {
+    eventName: "react workshiop",
+    eventMode: "online",
+    eventCapacity: 45
+};
+register(event1);
 //# sourceMappingURL=index.js.map
